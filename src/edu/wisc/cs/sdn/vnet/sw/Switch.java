98aea8358857aa749cc563b3c75ea168ed4c8211
Chris Lin <cblin@wisc.edu>
2026-02-25 14:53:41 -0600

    initial handlePacket

--- src/edu/wisc/cs/sdn/vnet/sw/Switch.java
+++ src/edu/wisc/cs/sdn/vnet/sw/Switch.java
@@ -3,6 +3,7 @@ package edu.wisc.cs.sdn.vnet.sw;
 import net.floodlightcontroller.packet.Ethernet;
 import net.floodlightcontroller.packet.MACAddress;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import edu.wisc.cs.sdn.vnet.Device;
@@ -13,63 +14,88 @@ import edu.wisc.cs.sdn.vnet.Iface;
  * @author Aaron Gember-Jacobson
  */
 public class Switch extends Device {
-	private class TableEntry<Iface, Date> {
-		private Iface iface;
-		private Date lastUsed;
 
-		public TableEntry(Iface iface, Date lastUsed) {
-			this.iface = iface;
-			this.lastUsed = lastUsed;
-		}
+    private final Long MAC_LEARNING_TIMEOUT_MS = new Long(15000);
 
-		public Iface getIface() {
-			return iface;
-		}
+    private class TableEntry {
+        private Iface iface;
+        private Long lastUsed;
 
-		public Date getLastUsed() {
+        public TableEntry(Iface iface, Long lastUsed) {
+            this.iface = iface;
+            this.lastUsed = lastUsed;
+        }
 
-		}
+        public Iface getIface() {
+            return iface;
+        }
 
-		public void setIface() {
+        public Long getLastUsed() {
+            return this.lastUsed;
+        }
 
-		}
+        public void setIface(Iface iface) {
+            this.iface = iface;
+        }
 
-		public void setLastUsed() {
+        public void setLastUsed(Long lastUsed) {
+            this.lastUsed = lastUsed;
+        }
+    }
 
-		}
-	}
+    /**
+     * Creates a router for a specific host.
+     * 
+     * @param host hostname for the router
+     */
+    Map<MACAddress, TableEntry> forwardingTable = new HashMap<>();
 
-	}/**
-		 * Creates a router for a specific host.
-		 * 
-		 * @param host hostname for the router
-		 */
-	Map<MACAddress,
+    public Switch(String host, DumpFile logfile) {
+        super(host, logfile);
+    }
 
-	public Switch(String host, DumpFile logfile) {
-		super(host, logfile);
-	}
+    /**
+     * Handle an Ethernet packet received on a specific interface.
+     * 
+     * @param etherPacket the Ethernet packet that was received
+     * @param inIface     the interface on which the packet was received
+     */
+    public void handlePacket(Ethernet etherPacket, Iface inIface) {
+        System.out.println("*** -> Received packet: " +
+                etherPacket.toString().replace("\n", "\n\t"));
+        Long currTime = System.currentTimeMillis();
+        forwardingTable.entrySet()
+                .removeIf(entry -> currTime - entry.getValue().getLastUsed() >= MAC_LEARNING_TIMEOUT_MS);
 
-	/**
-	 * Handle an Ethernet packet received on a specific interface.
-	 * 
-	 * @param etherPacket the Ethernet packet that was received
-	 * @param inIface     the interface on which the packet was received
-	 */
-	public void handlePacket(Ethernet etherPacket, Iface inIface) {
-		System.out.println("*** -> Received packet: " +
-				etherPacket.toString().replace("\n", "\n\t"));
+        /********************************************************************/
+        /* TODO: Handle packets */
+        MACAddress dst = etherPacket.getDestinationMAC();
+        MACAddress src = etherPacket.getSourceMAC();
+        if (forwardingTable.containsKey(src)) {
+            // update fwding tbl
+            TableEntry srcEntry = forwardingTable.get(src);
+            srcEntry.setLastUsed(System.currentTimeMillis());
+            srcEntry.setIface(inIface);
+        } else {
+            TableEntry entry = new TableEntry(inIface, System.currentTimeMillis());
+            forwardingTable.put(src, entry);
+        }
+        if (forwardingTable.containsKey(dst)) {
+            TableEntry dstTableEntry = forwardingTable.get(dst);
+            dstTableEntry.setLastUsed(System.currentTimeMillis());
+            Iface dstInterface = dstTableEntry.getIface();
+            sendPacket(etherPacket, dstInterface);
+            return;
+        }
 
-		/********************************************************************/
-		/* TODO: Handle packets */
-		MACAddress dst = etherPacket.getDestinationMAC();
-		MACAddress src = etherPacket.getSourceMAC();
+        // broadcast
+        for (Map.Entry<String, Iface> entry : interfaces.entrySet()) {
+            if (entry.getValue().equals(inIface))
+                continue;
+            sendPacket(etherPacket, entry.getValue());
+        }
 
-		sendPacket(etherPacket, inIface);
-		for (Map.Entry<String, Iface> entry : interfaces.entrySet()) {
-			sendPacket(entry.getValue());
-		}
-	}
+    }
 
-	/********************************************************************/
-}}
+    /********************************************************************/
+}
